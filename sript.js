d891bd30f066596ae0252dd274989dec13205277
Riswick <riswickelvariste@gmail.com>
2023-11-14 20:53:48 -0400

    homework with Array and set

---
+++ sript.js
@@ -0,0 +1,51 @@
+//console.log(numberDouble);
+let ecole = ["elvariste ", "riswick", "richecorve", "djimelove"];
+//console.log(ecole[3]);
+//console.log(ecole.length);
+//console.log(ecole[ecole.length - 1]);
+//let newArray = ecole.pop();
+//let newArray = ecole.shift();
+//console.log(newArray);
+
+//let addName = ecole.push("Jean");
+//console.log(ecole);
+
+//let sliceArray = ecole.slice(0, 2);
+//console.log(sliceArray);
+
+//console.log(ecole.indexOf("djimelove"))
+
+/*function include() {
+  if (ecole.includes("elvariste ")) {
+    console.log("this is the millonario");
+  } else {
+    console.log("you do not find elvariste");
+  }
+}
+include();
+ecole.push("riswick");
+
+console.log(ecole);
+let otherName = ecole.find((Element) => Element === "riswick");
+//console.log(otherName);
+
+let otherNamefilter = ecole.filter((element) => element === "riswick");
+console.log(otherNamefilter);*/
+
+let fruits = new Set([
+  "mango",
+  "fraise",
+  "raisin",
+  "ananas",
+  "bananas",
+  "bananas",
+]);
+
+//console.log(fruits.add("guayave"));
+//console.log(fruits.size);
+
+fruits.forEach((Element) => {
+  console.log(Element);
+});
+
+console.log(pay.delete("guayave"));
